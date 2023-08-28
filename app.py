eaeedf9ac70eb211bd7f79a5bfc6b952b8e3ab7f
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2023-08-28 10:32:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
+from st_files_connection import FilesConnection
 
 st.title("Law Assist")
 st.write("Ask me legal questions, and I'll provide answers!")
